efb6bfaf6a357d4e48d4ee19edd3ea2f4ab51db4
Daniel S. Billing <daniel.billing@intility.no>
2021-05-23 23:00:00 +0200

    Spelling

--- resources/views/certification/show.blade.php
+++ resources/views/certification/show.blade.php
@@ -41,7 +41,7 @@
                                 </tr>
                                 <tr class="border-b">
                                     <th scope="col" class="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                        NaIdentifierme
+                                        Identifier
                                     </th>
                                     <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-900 bg-white divide-y divide-gray-200">
                                         {{ $certification->identifier }}
