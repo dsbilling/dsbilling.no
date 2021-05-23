f824be728e5b3f960922d266dbfd6a1e1abfa58b
Daniel S. Billing <daniel.billing@intility.no>
2021-05-23 19:29:45 +0200

    Fixed favicons

--- resources/views/daniel.blade.php
+++ resources/views/daniel.blade.php
@@ -6,11 +6,13 @@
     <meta name="author" content="Daniel S. Billing">
 
     <title>Daniel S. Billing</title>
-
+    
+    <!-- FavIcon -->
     <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('favicon/apple-touch-icon.png') }}">
     <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('favicon/favicon-32x32.png') }}">
     <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('favicon/favicon-16x16.png') }}">
     <link rel="manifest" href="{{ asset('favicon/site.webmanifest') }}">
+    <link rel="icon" type="image/png" href="{{ asset('favicon/favicon.ico') }}">
 
     <link href="{{ mix('css/home.css') }}" rel="stylesheet">
 
