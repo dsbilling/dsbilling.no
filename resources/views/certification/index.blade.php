297929b51b2243b56f97a5dcb47c1766291bab50
Daniel S. Billing <daniel.billing@intility.no>
2021-05-23 22:02:23 +0200

    Added missing badge for certs without identifier

--- resources/views/certification/index.blade.php
+++ resources/views/certification/index.blade.php
@@ -59,7 +59,7 @@
                                             </td>
 
                                             <td class="px-6 py-4 whitespace-no-wrap text-sm leading-5 font-medium text-gray-900">
-                                                {{ $certification->identifier }}
+                                                {!! $certification->identifier ?? '<span class="inline-flex items-center justify-center px-2 py-1 text-xs font-bold leading-none text-red-100 bg-red-600 rounded-full">Missing</span>' !!}
                                             </td>
 
                                             <td class="px-6 py-4 whitespace-no-wrap text-sm leading-5 font-medium text-gray-900">
