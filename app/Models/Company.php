c95340c29460825e1c2d002087737979749d1ca3
Daniel S. Billing <daniel.billing@intility.no>
2021-05-23 18:07:18 +0200

    Formatting

--- app/Models/Company.php
+++ app/Models/Company.php
@@ -8,7 +8,8 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Company extends Model
 {
-    use HasFactory, SoftDeletes;
+    use HasFactory;
+    use SoftDeletes;
 
     protected $fillable = ['name'];
 
--- app/Models/Course.php
+++ app/Models/Course.php
@@ -8,7 +8,8 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Course extends Model
 {
-    use HasFactory, SoftDeletes;
+    use HasFactory;
+    use SoftDeletes;
 
     protected $fillable = [
         'name',
