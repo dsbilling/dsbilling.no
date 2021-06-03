7e93a00c19c41864015a364554e04e14ed011e67
Daniel S. Billing <daniel.billing@intility.no>
2021-06-03 22:22:00 +0200

    Added super-admin and headhunter middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -62,5 +62,7 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'super-admin' => \App\Http\Middleware\SuperAdminMiddleware::class,
+        'headhunter' => \App\Http\Middleware\HeadhunterMiddleware::class,
     ];
 }
