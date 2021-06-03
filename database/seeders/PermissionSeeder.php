6e24c7d7a566c76d8f07b14e7f8bbc1a9a75dc6c
Daniel S. Billing <daniel.billing@intility.no>
2021-06-03 21:19:42 +0200

    Update PermissionSeeder.php

--- database/seeders/PermissionSeeder.php
+++ database/seeders/PermissionSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
 use Spatie\Permission\PermissionRegistrar;
 
 class PermissionSeeder extends Seeder
@@ -19,7 +20,20 @@ class PermissionSeeder extends Seeder
         // Reset cached roles and permissions
         app()[PermissionRegistrar::class]->forgetCachedPermissions();
 
+        // Create permissions
+        Permission::create(['name' => 'timeline']);
+        Permission::create(['name' => 'cv']);
+        
+        // Create role and assign permission
+        $role = Role::create(['name' => 'headhunter']);
+        $role->givePermissionTo('timeline');
+        $role->givePermissionTo('cv');
+
+        // Super Admin
         $sa = Role::create(['name' => 'super-admin']);
+        $sa->givePermissionTo(Permission::all());
+
+        // Add role to private email
         $user = User::where('email', env('PRIVATE_EMAIL'))->firstOrFail();
         if($user) {
             $user->assignRole($sa);
