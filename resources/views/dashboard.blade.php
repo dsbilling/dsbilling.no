505a3f0b7e41b440182b1ef3ceeee7c36349769c
Daniel S. Billing <daniel.billing@intility.no>
2021-06-03 22:24:03 +0200

    Added permission checks to dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,68 +7,75 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="grid grid-cols-12 gap-6 mt-5">
-                <a class="transform  hover:scale-105 transition duration-300 shadow-xl rounded-lg col-span-12 sm:col-span-6 xl:col-span-3 intro-y bg-white"
-                    href="{{ route('courses.index') }}">
-                    <div class="p-5">
-                        <div class="ml-2 w-full flex-1">
-                            <div>
-                                <div class="mt-3 text-3xl font-bold leading-8">{{ $courses }}</div>
+            @role('super-admin')
+                <div class="grid grid-cols-12 gap-6 mt-5">
+                    <a class="transform  hover:scale-105 transition duration-300 shadow-xl rounded-lg col-span-12 sm:col-span-6 xl:col-span-3 intro-y bg-white"
+                        href="{{ route('courses.index') }}">
+                        <div class="p-5">
+                            <div class="ml-2 w-full flex-1">
+                                <div>
+                                    <div class="mt-3 text-3xl font-bold leading-8">{{ $courses }}</div>
 
-                                <div class="mt-1 text-base text-gray-600">Courses</div>
+                                    <div class="mt-1 text-base text-gray-600">Courses</div>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                </a>
-                <a class="transform  hover:scale-105 transition duration-300 shadow-xl rounded-lg col-span-12 sm:col-span-6 xl:col-span-3 intro-y bg-white"
-                    href="{{ route('certifications.index') }}">
-                    <div class="p-5">
-                        <div class="ml-2 w-full flex-1">
-                            <div>
-                                <div class="mt-3 text-3xl font-bold leading-8">{{ $certifications }}</div>
+                    </a>
+                    <a class="transform  hover:scale-105 transition duration-300 shadow-xl rounded-lg col-span-12 sm:col-span-6 xl:col-span-3 intro-y bg-white"
+                        href="{{ route('certifications.index') }}">
+                        <div class="p-5">
+                            <div class="ml-2 w-full flex-1">
+                                <div>
+                                    <div class="mt-3 text-3xl font-bold leading-8">{{ $certifications }}</div>
 
-                                <div class="mt-1 text-base text-gray-600">Certifications</div>
+                                    <div class="mt-1 text-base text-gray-600">Certifications</div>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                </a>
-                <a class="transform  hover:scale-105 transition duration-300 shadow-xl rounded-lg col-span-12 sm:col-span-6 xl:col-span-3 intro-y bg-white"
-                    href="{{ route('companies.index') }}">
-                    <div class="p-5">
-                        <div class="ml-2 w-full flex-1">
-                            <div>
-                                <div class="mt-3 text-3xl font-bold leading-8">{{ $companies }}</div>
+                    </a>
+                    <a class="transform  hover:scale-105 transition duration-300 shadow-xl rounded-lg col-span-12 sm:col-span-6 xl:col-span-3 intro-y bg-white"
+                        href="{{ route('companies.index') }}">
+                        <div class="p-5">
+                            <div class="ml-2 w-full flex-1">
+                                <div>
+                                    <div class="mt-3 text-3xl font-bold leading-8">{{ $companies }}</div>
 
-                                <div class="mt-1 text-base text-gray-600">Companies</div>
+                                    <div class="mt-1 text-base text-gray-600">Companies</div>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                </a>
-                <a class="transform  hover:scale-105 transition duration-300 shadow-xl rounded-lg col-span-12 sm:col-span-6 xl:col-span-3 intro-y bg-white"
-                    href="{{ route('experiences.index') }}">
-                    <div class="p-5">
-                        <div class="ml-2 w-full flex-1">
-                            <div>
-                                <div class="mt-3 text-3xl font-bold leading-8">{{ $experiences }}</div>
+                    </a>
+                    <a class="transform  hover:scale-105 transition duration-300 shadow-xl rounded-lg col-span-12 sm:col-span-6 xl:col-span-3 intro-y bg-white"
+                        href="{{ route('experiences.index') }}">
+                        <div class="p-5">
+                            <div class="ml-2 w-full flex-1">
+                                <div>
+                                    <div class="mt-3 text-3xl font-bold leading-8">{{ $experiences }}</div>
 
-                                <div class="mt-1 text-base text-gray-600">Experiences</div>
+                                    <div class="mt-1 text-base text-gray-600">Experiences</div>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                </a>
-                <a class="transform  hover:scale-105 transition duration-300 shadow-xl rounded-lg col-span-12 sm:col-span-6 xl:col-span-3 intro-y bg-white"
-                    href="{{ route('socials.index') }}">
-                    <div class="p-5">
-                        <div class="ml-2 w-full flex-1">
-                            <div>
-                                <div class="mt-3 text-3xl font-bold leading-8">{{ $socials }}</div>
+                    </a>
+                    <a class="transform  hover:scale-105 transition duration-300 shadow-xl rounded-lg col-span-12 sm:col-span-6 xl:col-span-3 intro-y bg-white"
+                        href="{{ route('socials.index') }}">
+                        <div class="p-5">
+                            <div class="ml-2 w-full flex-1">
+                                <div>
+                                    <div class="mt-3 text-3xl font-bold leading-8">{{ $socials }}</div>
 
-                                <div class="mt-1 text-base text-gray-600">Socials</div>
+                                    <div class="mt-1 text-base text-gray-600">Socials</div>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                </a>
-            </div>
+                    </a>
+                </div>
+            @else
+                <h1 class="text-3xl text-center md:text-5xl lg:text-6xl font-bold leading-tight">Welcome {{ auth()->user()->name }}!</h1>
+                @unlessrole('headhunter')
+                    <p class="text-1xl text-center py-3 font-bold leading-tight">Seems like you do not have permission from me to view anything yet. Please hold on, I'll notify you.</p>
+                @endunlessrole
+            @endrole
         </div>
     </div>
 </x-app-layout>
