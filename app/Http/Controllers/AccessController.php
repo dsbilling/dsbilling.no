9c1de37c2ca5e675b4734c58ec1932d554a3aec3
Daniel S. Billing <daniel.billing@intility.no>
2021-06-04 22:21:51 +0200

    Check if user already have access

--- app/Http/Controllers/AccessController.php
+++ app/Http/Controllers/AccessController.php
@@ -14,8 +14,14 @@ class AccessController extends Controller
      */
     public function wantAccess()
     {
-        $user = User::where('email', env('PRIVATE_EMAIL'))->first();
-        Notification::send($user, new WantAccess(auth()->user()));
+        $user = auth()->user();
+        if ($user->hasRole('headhunter')) {
+            session()->flash('flash.banner', 'You already have access!');
+            session()->flash('flash.bannerStyle', 'success');
+            return redirect()->route('dashboard');
+        }
+        $sendto = User::where('email', env('PRIVATE_EMAIL'))->first();
+        Notification::send($sendto, new WantAccess($user));
         session()->flash('flash.banner', 'Notification sent!');
         session()->flash('flash.bannerStyle', 'success');
         return redirect()->route('dashboard');
