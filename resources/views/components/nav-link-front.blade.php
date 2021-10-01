dbc0154edd53ab975e672b65614374b6b92b8dd7
Daniel S. Billing <daniel.billing@intility.no>
2021-10-01 21:58:40 +0200

    Update nav-link-front.blade.php

--- resources/views/components/nav-link-front.blade.php
+++ resources/views/components/nav-link-front.blade.php
@@ -2,8 +2,8 @@
 
 @php
 $classes = ($active ?? false)
-            ? 'px-3 py-2 transition-colors text-red-200 hover:text-red-400 focus:text-red-400 focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-blue-500 focus-visible:ring-opacity-60 focus-visible:outline-none focus:outline-none'
-            : 'px-3 py-2 transition-colors text-gray-800 hover:text-red-400 focus:text-red-400 focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-blue-500 focus-visible:ring-opacity-60 focus-visible:outline-none focus:outline-none';
+            ? 'px-3 py-2 transition-colors text-red-200 dark:text-red-100 hover:text-red-400 focus:text-red-400 focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-blue-500 focus-visible:ring-opacity-60 focus-visible:outline-none focus:outline-none'
+            : 'px-3 py-2 transition-colors text-gray-800 dark:text-gray-100 hover:text-red-400 focus:text-red-400 focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-blue-500 focus-visible:ring-opacity-60 focus-visible:outline-none focus:outline-none';
 @endphp
     
 <a {{ $attributes->merge(['class' => $classes]) }}>
