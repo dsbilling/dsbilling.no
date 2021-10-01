96cd6fd2e22498becf07be6799020ca2de014682
Daniel S. Billing <daniel.billing@intility.no>
2021-10-01 21:58:35 +0200

    Added responsive menu component

---
+++ resources/views/components/nav-link-responsive-front.blade.php
@@ -0,0 +1,11 @@
+@props(['active'])
+
+@php
+$classes = ($active ?? false)
+            ? 'block px-3 py-2 text-base font-medium text-red-400 rounded-md hover:text-gray-800 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-gray-100'
+            : 'block px-3 py-2 text-base font-medium text-gray-800 dark:text-gray-100 rounded-md hover:text-red-400 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-red-200';
+@endphp
+    
+<a {{ $attributes->merge(['class' => $classes]) }}>
+    {{ $slot }}
+</a>
