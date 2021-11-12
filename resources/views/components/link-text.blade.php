cd67661818f16245d9f25426d9fd2ba84eea5f0f
Daniel S. Billing <daniel.billing@intility.no>
2021-11-12 23:39:00 +0100

    Added link text component

---
+++ resources/views/components/link-text.blade.php
@@ -0,0 +1,1 @@
+<a href="{{ $link }}" class="font-bold transition-colors rounded hover:underline hover:text-yellow-500 focus:text-red-500 focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-red-500 focus-visible:ring-opacity-60 focus-visible:outline-none focus:outline-none" rel="noreferrer">{{ $slot }}</a>
