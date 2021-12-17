d6aa20060091c2a25db41ccc5467918640d42542
Daniel S. Billing <daniel.billing@intility.no>
2021-12-17 20:36:05 +0100

    Create BlogController.php

---
+++ app/Http/Controllers/BlogController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class BlogController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $posts = Post::where('published_at', '<=', now())->where('draft', false)->orderByDesc('published_at')->paginate(5);
+        return view('blog.index', ['posts' => $posts]);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function show($param)
+    {
+        $post = Post::where('uuid', $param)
+            ->where('draft', false)
+            ->orWhere('slug', $param)
+            ->where('draft', false)
+            ->firstOrFail();
+        abort_if(!$post, 404);
+        views($post)->cooldown(60)->record();
+        $html = app(\Spatie\LaravelMarkdown\MarkdownRenderer::class)
+                ->toHtml($post->body);
+        return view('blog.show', ['post' => $post, 'html' => $html]);
+    }
+}
