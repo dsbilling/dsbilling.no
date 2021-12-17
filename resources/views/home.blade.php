10f9a554671e83c9b8a8e29d57914d50f52a30d2
Daniel S. Billing <daniel.billing@intility.no>
2021-12-17 20:41:49 +0100

    Added blog posts to home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -56,34 +56,41 @@
                 </div>
             </section>
 
-            {{--
             <section id="blog">
                 <div class="container mx-auto">
                     <h2 class="text-2xl font-bold text-gray-800 dark:text-gray-200">Recent Posts</h2>
                     <h4 class="mt-2 text-gray-500 dark:text-gray-400">Thoughts on what I'm building and learning.</h4>
-                    <div class="pt-10 sm:grid sm:grid-cols-2 sm:gap-10">
+                    <div class="grid gap-4 pt-10 sm:gap-6 md:grid-cols-3 lg:gap-10">
 
-                        <div class="m-auto overflow-hidden rounded-lg shadow-lg cursor-pointer group">
-                            <a href="#" class="block w-full h-full">
-                                <img alt="blog photo" src="{{ asset('img/daniel.jpg') }}" class="object-cover w-full max-h-40"/>
-                                <div class="w-full p-4 bg-white dark:bg-gray-800">
-                                    <p class="text-sm font-medium text-indigo-500">
-                                        1 min read
-                                    </p>
-                                    <p class="mb-2 text-xl font-medium text-gray-800 dark:text-white group-hover:text-red-500">
-                                        How I Built My Site with Laravel
-                                    </p>
-                                    <p class="font-light text-gray-400 dark:text-gray-300 text-md">
-                                        OMG, this is how I built my site with Laravel. I'm so proud of this. And here is how I built it. It is so easy to learn and use.
-                                    </p>
-                                </div>
-                            </a>
-                        </div>
+                        @foreach ($posts as $post)
+                            <div class="m-auto overflow-hidden rounded-lg shadow-lg cursor-pointer group">
+                                <a href="{{ route('blog.show', $post->slug) }}" class="block w-full h-full">
+                                    {{--<img alt="blog photo" src="{{ asset('img/daniel.jpg') }}" class="object-cover w-full max-h-28"/>--}}
+                                    <div class="w-full p-4 bg-white dark:bg-gray-800 hover:dark:bg-gray-700">
+                                        <p class="text-sm font-medium text-orange-400">
+                                            <svg xmlns="http://www.w3.org/2000/svg" class="inline-block w-4 h-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253" />
+                                            </svg> {{ read_time($post->body) }} &middot; 
+                                            <svg xmlns="http://www.w3.org/2000/svg" class="inline-block w-4 h-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+                                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
+                                            </svg> {{ App\Helpers\NumberHelper::nearestK(views($post)->count()) }}
+                                        </p>
+                                        <p class="mb-2 text-2xl font-semibold text-gray-800 dark:text-white">
+                                            {{ $post->title }}
+                                        </p>
+                                        <p class="text-sm font-light text-gray-400 dark:text-gray-300">
+                                            {!! strip_tags(Str::of(Str::limit($post->body, 300))->markdown()) !!}
+                                        </p>
+                                    </div>
+                                </a>
+                            </div>
+                        @endforeach
 
                     </div>
                 </div>
             </section>
-            --}}
+            
         </div>
     </main>
     <div class="pb-6 mt-24">
