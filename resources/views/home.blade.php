6714b0f3135637ea98217ff0f73abafc870b19ca
Daniel S. Billing <daniel.billing@intility.no>
2021-12-17 23:22:41 +0100

    Removed icons, plain text now

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -26,14 +26,8 @@
                             <a href="{{ route('blog.show', $post->slug) }}" class="block w-full h-full">
                                 {{--<img alt="blog photo" src="{{ asset('img/daniel.jpg') }}" class="object-cover w-full max-h-28"/>--}}
                                 <div class="w-full p-4 bg-white dark:bg-gray-800 hover:dark:bg-gray-700">
-                                    <p class="text-sm font-medium text-orange-400">
-                                        <svg xmlns="http://www.w3.org/2000/svg" class="inline-block w-4 h-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253" />
-                                        </svg> {{ read_time($post->body) }} &middot; 
-                                        <svg xmlns="http://www.w3.org/2000/svg" class="inline-block w-4 h-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
-                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
-                                        </svg> {{ App\Helpers\NumberHelper::nearestK(views($post)->count()) }}
+                                    <p class="text-xs font-medium text-orange-500 dark:text-orange-400">
+                                        {{ $post->user->name }} &middot; {{ $post->published_at->diffForHumans() }} &middot; {{ read_time($post->body)}} &middot;  {{ App\Helpers\NumberHelper::nearestK(views($post)->count()) }} {{ Str::plural('view', views($post)->count()) }}
                                     </p>
                                     <p class="my-2 text-xl font-semibold text-gray-800 truncate dark:text-white">
                                         {{ $post->title }}
@@ -45,7 +39,6 @@
                             </a>
                         </div>
                     @endforeach
-
                 </div>
             </section>
         @endif
