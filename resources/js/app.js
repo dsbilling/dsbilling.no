002eb06884d79aedc83f7fd55bbbc88db5d8e557
Daniel S. Billing <daniel.billing@intility.no>
2021-12-17 20:39:01 +0100

    Updated alpine import, removed FA, added flatpickr

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,16 @@
 require('./bootstrap');
 
-require('alpinejs');
+import Alpine from 'alpinejs';
+window.Alpine = Alpine;
+Alpine.start();
 
-require('@fortawesome/fontawesome-free/js/all.js');
+//require('@fortawesome/fontawesome-free/js/all.js');
+
+import flatpickr from "flatpickr";
+require("flatpickr/dist/themes/dark.css");
+
+flatpickr(".flatpickrSelector", {
+    enableTime: true,
+    dateFormat: "Y-m-d H:i:S",
+    time_24hr: true,
+});
