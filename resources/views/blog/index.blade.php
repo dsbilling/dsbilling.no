04a089f48c2e1a6c23e66abf7a000246b393feb4
Daniel S. Billing <daniel.billing@intility.no>
2022-01-21 21:50:34 +0100

    Added title to blog page

--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -1,4 +1,8 @@
 <x-guest-layout>
+	<div class="mb-4">
+		<h2 class="text-xl font-bold text-gray-800 dark:text-gray-200">Recent Posts</h2>
+		<h4 class="mt-2 text-gray-500 dark:text-gray-400 text-md">My thoughts on what I'm building and learning.</h4>
+	</div>
 	<div class="flex flex-col items-center">
 		<div class="grid w-full grid-cols-1 gap-8">
 			@forelse($posts as $post)
