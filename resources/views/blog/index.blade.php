d8ca6e65047196ff2097763259e5b0a2e00a8025
Daniel S. Billing <daniel.billing@intility.no>
2022-01-21 21:39:38 +0100

    Updated styling for blog post cards

--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -2,11 +2,11 @@
 	<div class="flex flex-col items-center">
 		<div class="grid w-full grid-cols-1 gap-8">
 			@forelse($posts as $post)
-				<div class="w-full m-auto overflow-hidden rounded-lg shadow-lg cursor-pointer group">
+				<div class="w-full m-auto overflow-hidden border border-gray-200 rounded-lg shadow-lg cursor-pointer bg-gray-50 group dark:bg-gray-800 hover:dark:bg-gray-700 dark:border-gray-700">
 					<a href="{{ route('blog.show', $post->slug) }}" class="block w-full h-full">
 						{{--<img alt="blog photo" src="{{ asset('img/daniel.jpg') }}" class="object-cover w-full max-h-28"/>--}}
-						<div class="w-full p-4 bg-white dark:bg-gray-800 hover:dark:bg-gray-700">
-							<p class="text-sm font-medium text-orange-400">
+						<div class="w-full p-4 ">
+							<p class="text-sm font-medium text-orange-500 dark:text-orange-400">
 								{{ $post->user->name }} &middot; {{ now()->subMonth(1) > $post->published_at ? $post->published_at->isoFormat('D MMMM YYYY') : $post->published_at->diffForHumans() }} &middot; {{ read_time($post->body)}} &middot;  {{ App\Helpers\NumberHelper::nearestK(views($post)->count()) }} {{ Str::plural('view', views($post)->count()) }} &middot; {{ $post->likes_count }} {{ Str::plural('like', $post->likes_count) }}
 							</p>
 							<p class="my-2 text-2xl font-semibold text-gray-800 sm:text-3xl dark:text-white">
