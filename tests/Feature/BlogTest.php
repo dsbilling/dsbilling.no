f7e11c8405844b9103fd9eaf15a0b1c83e75d01c
Daniel S. Billing <daniel.billing@intility.no>
2022-01-22 20:11:32 +0100

    Update BlogTest.php

--- tests/Feature/BlogTest.php
+++ tests/Feature/BlogTest.php
@@ -3,24 +3,18 @@
 use App\Models\Post;
 use function Pest\Laravel\get;
 
-beforeEach(fn () => Post::factory()->create(['title' => 'Post 1', 'slug' => 'post-1']));
-
 it('has blog page', function () {
     get('/blog')->assertStatus(200);
 });
 
-it('has posts')->assertDatabaseHas('posts', [
-    'slug' => 'post-1',
-]);
-
 it('can see post on blog page', function () {
-    expect(get('/blog'))
+    $post = Post::factory()->create();
+    get('/blog')
         ->assertStatus(200)
-        ->assertSee('Post 1');
+        ->assertSeeText($post->title);
 });
 
 it('can see post page', function () {
-    expect(get('/blog/post-1'))
-        ->assertStatus(200)
-        ->assertSee('Post 1');
+    $post = Post::factory()->create();
+    get('/blog/'.$post->slug)->assertStatus(200);
 });
