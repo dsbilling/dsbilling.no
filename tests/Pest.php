4ba705e372bc175dbd8a3e91d7ec8b97f1ed66e8
Daniel S. Billing <daniel.billing@intility.no>
2022-01-22 18:22:02 +0100

    RefreshDatabase

--- tests/Pest.php
+++ tests/Pest.php
@@ -1,5 +1,9 @@
 <?php
 
+use Illuminate\Foundation\Testing\RefreshDatabase;
+ 
+uses(RefreshDatabase::class)->in('Feature');
+
 /*
 |--------------------------------------------------------------------------
 | Test Case
