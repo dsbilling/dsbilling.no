412565b60cc7afddaad5253fbc68f48707c0b021
Daniel S. Billing <daniel.billing@intility.no>
2022-02-22 12:48:29 +0100

    Update BlogController.php

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -15,7 +15,7 @@ class BlogController extends Controller
      */
     public function index()
     {
-        seo()->title('Blog - '.config('app.name'));
+        seo()->title('Blog - ' . config('app.name'));
         $posts = Post::where('published_at', '<=', now())->where('draft', false)->orderByDesc('published_at')->paginate(5);
         return view('blog.index', ['posts' => $posts]);
     }
@@ -35,12 +35,10 @@ class BlogController extends Controller
             ->firstOrFail();
         abort_if(!$post, 404);
         views($post)->cooldown(60)->record();
-        $html = app(\Spatie\LaravelMarkdown\MarkdownRenderer::class)
-                ->toHtml($post->body);
 
-        seo()->title($post->title.' - Blog - '.config('app.name'));
+        seo()->title($post->title . ' - Blog - ' . config('app.name'));
         seo()->description(strip_tags(Str::of(Str::limit($post->body, 150))->markdown()));
 
-        return view('blog.show', ['post' => $post, 'html' => $html]);
+        return view('blog.show', ['post' => $post]);
     }
 }
