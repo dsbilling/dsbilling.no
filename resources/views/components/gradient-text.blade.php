54194ca0fc5c2811c258822e44dcc1bfc667bba1
Daniel S. Billing <daniel@kilobyte.no>
2022-03-01 21:43:27 +0100

    🇺🇦 ❤️

--- resources/views/components/gradient-text.blade.php
+++ resources/views/components/gradient-text.blade.php
@@ -1,1 +1,2 @@
-<span class="font-bold text-transparent bg-clip-text bg-gradient-to-r dark:from-yellow-400 dark:to-red-400 from-red-600 to-red-400">{{ $slot }}</span>
+<span class="font-bold text-transparent bg-clip-text bg-gradient-to-r from-blue-600 to-yellow-400">{{ $slot }}</span>
+{{-- dark:from-yellow-400 dark:to-red-400 from-red-600 to-red-400 --}}
