7a1951eedeaa399dd2b02e8b428a8fe14b3ebbf6
Daniel S. Billing <daniel@kilobyte.no>
2022-06-28 18:07:48 +0200

    Updated link text

--- resources/views/components/link-text.blade.php
+++ resources/views/components/link-text.blade.php
@@ -1,1 +1,9 @@
-<a href="{{ $link }}" class="font-bold transition-colors rounded hover:underline hover:text-yellow-500 focus:text-red-500 focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-red-500 focus-visible:ring-opacity-60 focus-visible:outline-none focus:outline-none" rel="noreferrer" target="blank">{{ $slot }}</a>
+@props(['noreferrer', 'link'])
+
+@php
+    $ref = ($noreferrer ?? false)
+                ? 'noreferrer'
+                : 'external';
+@endphp
+
+<a href="{{ $link }}" class="font-bold transition-colors rounded hover:underline hover:text-yellow-500 focus:text-red-500 focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-red-500 focus-visible:ring-opacity-60 focus-visible:outline-none focus:outline-none" rel="{{ $ref }}" target="blank">{{ $slot }}</a>
