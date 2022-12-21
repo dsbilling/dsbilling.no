76041681d400755fe1ad7e09c2aadc3597fd2834
Daniel S. Billing <daniel@izy.no>
2022-12-21 14:22:28 +0100

    Changed indigo with orange

--- resources/views/vendor/jetstream/components/checkbox.blade.php
+++ resources/views/vendor/jetstream/components/checkbox.blade.php
@@ -1,1 +1,1 @@
-<input type="checkbox" {!! $attributes->merge(['class' => 'rounded border-gray-300 text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50']) !!}>
+<input type="checkbox" {!! $attributes->merge(['class' => 'rounded border-gray-300 text-orange-600 shadow-sm focus:border-orange-300 focus:ring focus:ring-orange-200 focus:ring-opacity-50']) !!}>
