8f5ef0672fa6078e30147f17adab4e77f9585328
Daniel S. Billing <daniel@izy.no>
2023-06-26 17:29:42 +0200

    Update Like.php

--- app/Http/Livewire/Like.php
+++ app/Http/Livewire/Like.php
@@ -3,35 +3,33 @@
 namespace App\Http\Livewire;
 
 use App\Models\Post;
+use App\Models\User;
 use Livewire\Component;
 
 class Like extends Component
 {
     public Post $post;
-
     public int $count;
+    public User|null $user = null;
 
-    public $user;
-
-    public function mount(Post $post)
+    public function mount(Post $post): void
     {
         $this->post = $post;
         $this->count = $post->likes()->count();
         $this->user = auth()->check() ? auth()->user() : null;
     }
 
-    public function like()
+    public function like(): void
     {
         if ($this->post->hasLiked($this->user)) {
             $this->post->dislike($this->user);
-            $this->count = $this->post->likes()->count();
         } else {
             $this->post->like($this->user);
-            $this->count = $this->post->likes()->count();
         }
+        $this->count = $this->post->likes()->count();
     }
 
-    public function render()
+    public function render(): \Illuminate\Contracts\View\View
     {
         return view('livewire.like');
     }
