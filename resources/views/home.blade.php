31e3d07709df16a5856397f5858a8aa5769fcf37
Daniel S. Billing <daniel@izy.no>
2023-12-01 10:58:09 +0100

    WIP Redesign

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,56 +1,128 @@
+@php use App\Models\Social; @endphp
 <x-guest-layout>
-    <div class="px-4 space-y-14 lg:space-y-24">
-        <section id="about">
-            <div class="space-x-0 sm:space-x-5 lg:flex item-center lg:-mx-4">
-                <div>
-                    <h1 class="text-3xl font-bold text-gray-900 dark:text-gray-50 lg:text-4xl">Hi there, I'm Daniel &#x1F44B;</h1>
-                    <div class="mt-4 text-gray-800 dark:text-gray-100">
-                        <p>I currently work as a <x-gradient-text>Backend Lead</x-gradient-text> for <x-link-text link="https://izy.no" noreferrer="true">Izy AS</x-link-text>, where we develop platform for landlords to take care of tenants' needs in a digital everyday life, with an associated mobile application for communication, trade, information and booking.</p>
-                        <p class="mt-2">In January 2022, I started my own company called <x-link-text link="https://kilobyte.no"><x-gradient-text>Kilobyte AS</x-gradient-text></x-link-text>, a technology and development company. Before this I had a sole proprietorship for 10 years.</p>
-                        <p class="mt-2">I previously worked 10 years at <x-link-text link="https://intility.no" noreferrer="true">Intility AS</x-link-text> as a <x-gradient-text>Systems Developer</x-gradient-text> in the <x-gradient-text>AV</x-gradient-text> department &mdash; focusing on developing an internal portal, but I also do develop solutions for digital signage as well as programming meeting rooms, auditoriums, and operation centers with <x-link-text link="https://crestron.com" noreferrer="true">Crestron</x-link-text>.</p>
-                        <p class="mt-2">Other than that I do <x-gradient-text>freelance work</x-gradient-text> for <x-gradient-text>e-sport</x-gradient-text> teams, <x-gradient-text>LAN-parties</x-gradient-text>, <x-gradient-text>games</x-gradient-text> and <x-gradient-text>digital communities</x-gradient-text>. <span class="pb-0 border-b border-gray-400 border-dashed dark:border-gray-500">I love making websites that are usable, simple, and user-friendly.</span></p>
-                        <p class="mt-2">If you want to get in touch with me use one of my socials. <x-gradient-text>Say hi!</x-gradient-text></p>
-                    </div>
-                </div>
-                <div class="items-center flex-shrink-0 mx-auto mt-12 lg:px-4 lg:mt-0 sm:mx-0">
-                    <img alt="Profile" src="{{ asset('img/daniel.jpg') }}" class="flex w-56 h-56 mx-auto rounded-full sm:mx-0">
-                </div>
-            </div>
-        </section>
 
-        @if($posts->count() > 0)
-            <section id="blog" class=" lg:-mx-4">
-                <h2 class="text-xl font-bold text-gray-800 dark:text-gray-200">Recent Posts</h2>
-                <h4 class="mt-2 text-gray-500 dark:text-gray-400 text-md">My thoughts on what I'm building and learning.</h4>
-                <div class="grid gap-4 pt-8 sm:gap-6 md:grid-cols-2 lg:gap-8">
-                    @foreach ($posts as $post)
-                        <div class="w-full h-full m-auto overflow-hidden border border-gray-100 rounded-lg shadow-lg cursor-pointer bg-gray-50/50 hover:bg-gray-50 dark:border-gray-700 group dark:bg-gray-800/50 hover:dark:bg-gray-800 dark:shadow-gray-100/10">
-                            <a href="{{ route('blog.show', $post->slug) }}" class="block w-full h-full">
-                                {{--<img alt="blog photo" src="{{ asset('img/daniel.jpg') }}" class="object-cover w-full max-h-28"/>--}}
-                                <div class="w-full p-4">
-                                    <p class="text-xs font-medium text-orange-500 dark:text-orange-400">
-                                        {{ $post->published_at->diffForHumans() }} &middot; {{ read_time($post->body)}} &middot;  {{ App\Helpers\NumberHelper::nearestK(views($post)->count()) }} {{ Str::plural('view', views($post)->count()) }} &middot; {{ $post->likes_count }} {{ Str::plural('like', $post->likes_count) }}
-                                    </p>
-                                    <p class="my-2 text-xl font-semibold text-gray-800 truncate dark:text-white">
-                                        {{ $post->title }}
-                                    </p>
-                                    <p class="text-sm font-light text-gray-400 dark:text-gray-300">
-                                        {!! strip_tags(Str::of(Str::limit($post->body, 300))->markdown()) !!}
-                                    </p>
-                                </div>
+    <div class="mx-auto max-w-2xl lg:max-w-5xl">
+        <div class="max-w-2xl">
+            <h1 class="text-4xl font-bold tracking-tight text-slate-800 dark:text-slate-100 sm:text-5xl">
+                Backend Lead and Senior Fullstack Developer
+            </h1>
+            <p class="mt-6 text-base text-slate-600 dark:text-slate-300">
+                Hello! I am Daniel, a Backend Lead and Senior Fullstack Developer from Norway. I have a passion for
+                creating and developing digital solutions. Get to know
+                <x-link-text link="">
+                    <x-gradient-text>about me</x-gradient-text>
+                </x-link-text>
+                or
+                <x-link-text link="">
+                    <x-gradient-text>get in touch</x-gradient-text>
+                </x-link-text>
+                !
+            </p>
+            <div class="mt-6 flex gap-6">
+                @foreach (Social::all() as $social)
+                    <a class="group -m-1 p-1 text-slate-500 dark:text-slate-200 hover:text-slate-400" aria-label="Follow on {{$social->name}}"
+                       href="{{$social->link}}">
+                        <i class="{{$social->icon}} text-sm mr-1"></i>{{$social->name}}
+                    </a>
+                @endforeach
+            </div>
+        </div>
+    </div>
+
+    <div class="sm:px-8 mt-24 md:mt-28">
+        <div class="mx-auto max-w-2xl lg:max-w-5xl grid grid-cols-1 gap-y-20 lg:grid-cols-2">
+            <section id="blog" class="flex flex-col gap-16">
+                @foreach ($posts as $post)
+                    <article class="group relative flex flex-col items-start">
+                        <h2 class="text-base font-bold tracking-tight text-slate-800 dark:text-slate-100">
+                            <div class="absolute -inset-x-4 -inset-y-6 z-0 scale-95 bg-slate-50 opacity-0 transition group-hover:scale-100 group-hover:opacity-100 dark:bg-slate-800/50 sm:-inset-x-6 sm:rounded-2xl"></div>
+                            <a href="{{ route('blog.show', $post->slug) }}">
+                                <span class="absolute -inset-x-4 -inset-y-6 z-20 sm:-inset-x-6 sm:rounded-2xl"></span>
+                                <span class="relative z-10">{{ $post->title }}</span>
                             </a>
+                        </h2>
+                        <time
+                            class="relative z-10 order-first mb-3 flex items-center text-sm text-slate-500 dark:text-slate-400 pl-3.5"
+                            datetime="{{ $post->published_at->format('Y-m-d') }}">
+                            <span class="absolute inset-y-0 left-0 flex items-center" aria-hidden="true">
+                                <span class="h-4 w-0.5 rounded-full bg-slate-300 dark:bg-slate-400"></span>
+                            </span>
+                            {{ now()->subMonth(1) > $post->published_at ? $post->published_at->isoFormat('D. MMMM YYYY') : $post->published_at->diffForHumans() }}
+                        </time>
+                        <p class="relative z-10 mt-2 text-sm text-slate-600 dark:text-slate-400">{!! strip_tags(Str::of(Str::limit($post->body, 300))->markdown()) !!}</p>
+                        <div aria-hidden="true" class="relative z-10 mt-4 flex items-center text-sm font-medium text-orange-500 dark:text-orange-400">
+                            Read article
+                            <svg viewBox="0 0 16 16" fill="none" aria-hidden="true" class="ml-1 h-4 w-4 stroke-current">
+                                <path d="M6.75 5.75 9.25 8l-2.5 2.25" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
+                            </svg>
                         </div>
-                    @endforeach
-                </div>
-                @if($poststotalcount >= 4)
-                    <h6 class="mt-8 text-gray-500 dark:text-gray-400 text-sm">
-                        I have written {{ $poststotalcount }} {{ Str::plural('post', $poststotalcount) }} so
-                        far, read more of them <x-link-text link="{{ route('blog.index') }}" referrer="false" target="_self"><x-gradient-text>here
-                                &rarr;
-                            </x-gradient-text></x-link-text>
-                    </h6>
-                @endif
+                    </article>
+                @endforeach
             </section>
-        @endif
+            <section class="space-y-10 lg:pl-16 xl:pl-24">
+                {{--<form class="rounded-2xl border border-slate-100 p-6 dark:border-slate-700/40" action="/thank-you"
+                      data-dashlane-rid="424ef476045e3cd4" data-form-type="other">
+                    <h2 class="flex text-sm font-semibold text-slate-900 dark:text-slate-100">
+                        <svg viewBox="0 0 24 24" fill="none" stroke-width="1.5" stroke-linecap="round"
+                             stroke-linejoin="round" aria-hidden="true" class="h-6 w-6 flex-none">
+                            <path
+                                d="M2.75 7.75a3 3 0 0 1 3-3h12.5a3 3 0 0 1 3 3v8.5a3 3 0 0 1-3 3H5.75a3 3 0 0 1-3-3v-8.5Z"
+                                class="fill-slate-100 stroke-slate-400 dark:fill-slate-100/10 dark:stroke-slate-500"></path>
+                            <path d="m4 6 6.024 5.479a2.915 2.915 0 0 0 3.952 0L20 6"
+                                  class="stroke-slate-400 dark:stroke-slate-500"></path>
+                        </svg>
+                        <span class="ml-3">Stay up to date</span></h2>
+                    <p class="mt-2 text-sm text-slate-600 dark:text-slate-400">Get notified when I publish something new,
+                        and unsubscribe at any time.</p>
+                    <div class="mt-6 flex"><input type="email" placeholder="Email address" aria-label="Email address"
+                                                  required=""
+                                                  class="min-w-0 flex-auto appearance-none rounded-md border border-slate-900/10 bg-white px-3 py-[calc(theme(spacing.2)-1px)] shadow-md shadow-slate-800/5 placeholder:text-slate-400 focus:border-teal-500 focus:outline-none focus:ring-4 focus:ring-teal-500/10 dark:border-slate-700 dark:bg-slate-700/[0.15] dark:text-slate-200 dark:placeholder:text-slate-500 dark:focus:border-teal-400 dark:focus:ring-teal-400/10 sm:text-sm"
+                                                  data-dashlane-rid="6ef8a63ba5cb8058" data-form-type="email"
+                                                  data-kwimpalastatus="alive" data-kwimpalaid="1701418315135-0">
+                        <button
+                            class="inline-flex items-center gap-2 justify-center rounded-md py-2 px-3 text-sm outline-offset-2 transition active:transition-none bg-slate-800 font-semibold text-slate-100 hover:bg-slate-700 active:bg-slate-800 active:text-slate-100/70 dark:bg-slate-700 dark:hover:bg-slate-600 dark:active:bg-slate-700 dark:active:text-slate-100/70 ml-4 flex-none"
+                            type="submit" data-dashlane-rid="4c663953f9d172f7" data-form-type="other"
+                            data-dashlane-label="true">Join
+                        </button>
+                    </div>
+                </form>--}}
+                <div class="rounded-2xl border border-slate-100 p-6 dark:border-slate-700/40">
+                    <h2 class="flex text-sm font-semibold text-slate-900 dark:text-slate-100">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 flex-none">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M20.25 14.15v4.25c0 1.094-.787 2.036-1.872 2.18-2.087.277-4.216.42-6.378.42s-4.291-.143-6.378-.42c-1.085-.144-1.872-1.086-1.872-2.18v-4.25m16.5 0a2.18 2.18 0 00.75-1.661V8.706c0-1.081-.768-2.015-1.837-2.175a48.114 48.114 0 00-3.413-.387m4.5 8.006c-.194.165-.42.295-.673.38A23.978 23.978 0 0112 15.75c-2.648 0-5.195-.429-7.577-1.22a2.016 2.016 0 01-.673-.38m0 0A2.18 2.18 0 013 12.489V8.706c0-1.081.768-2.015 1.837-2.175a48.111 48.111 0 013.413-.387m7.5 0V5.25A2.25 2.25 0 0013.5 3h-3a2.25 2.25 0 00-2.25 2.25v.894m7.5 0a48.667 48.667 0 00-7.5 0M12 12.75h.008v.008H12v-.008z" />
+                        </svg>
+                        <span class="ml-3">Experience</span>
+                    </h2>
+                    <ol class="mt-6 space-y-4">
+                        <li class="flex gap-4">
+                            <div class="relative mt-1 flex h-10 w-10 flex-none items-center justify-center rounded-full shadow-md shadow-slate-800/5 ring-1 ring-slate-900/5 dark:border dark:border-slate-700/50 dark:bg-slate-800 dark:ring-0">
+                                <img alt="" loading="lazy" width="32" height="32" decoding="async" data-nimg="1" class="h-7 w-7" style="color:transparent" src="">
+                            </div>
+                            <dl class="flex flex-auto flex-wrap gap-x-2">
+                                <dt class="sr-only">Company</dt>
+                                <dd class="w-full flex-none text-sm font-medium text-slate-900 dark:text-slate-100">
+                                    Kilobyte AS
+                                </dd>
+                                <dt class="sr-only">Role</dt>
+                                <dd class="text-xs text-slate-500 dark:text-slate-400">Founder & Lead Developer</dd>
+                                <dt class="sr-only">Date</dt>
+                                <dd class="ml-auto text-xs text-slate-400 dark:text-slate-500"
+                                    aria-label="2019 until Present">
+                                    <time datetime="01-2022">Feb 2022</time>
+                                    <span aria-hidden="true">&mdash;</span>
+                                    <time datetime="2023">Present</time>
+                                </dd>
+                            </dl>
+                        </li>
+                    </ol>
+                    <a class="inline-flex items-center gap-2 justify-center rounded-md py-2 px-3 text-sm outline-offset-2 transition active:transition-none bg-slate-50 font-medium text-slate-900 hover:bg-slate-100 active:bg-slate-100 active:text-slate-900/60 dark:bg-slate-800/50 dark:text-slate-300 dark:hover:bg-slate-800 dark:hover:text-slate-50 dark:active:bg-slate-800/50 dark:active:text-slate-50/70 group mt-6 w-full"
+                       href="#">Download my CV
+                        <svg aria-hidden="true"  xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="h-4 w-4 stroke-slate-400 transition group-active:stroke-slate-600 dark:group-hover:stroke-slate-50 dark:group-active:stroke-slate-50">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5M16.5 12L12 16.5m0 0L7.5 12m4.5 4.5V3" />
+                        </svg>
+                    </a>
+                </div>
+            </section>
+        </div>
     </div>
 </x-guest-layout>
--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="h-full antialiased">
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -27,58 +27,69 @@
             <script defer data-domain="dsbilling.no" src="https://plausible.io/js/plausible.js"></script>
         @endproduction
     </head>
-    <body class="h-screen min-h-full {{ config('app.debug') ? 'debug-screens' : '' }}">
-        <div class="min-h-full px-4 pb-8 font-sans antialiased text-gray-900 bg-white dark:bg-gray-900">
-            <nav x-data="{ open: false }" class="px-4 bg-white md:px-8 dark:bg-gray-900">
-                <div class="max-w-4xl mx-auto">
-                    <div class="flex items-center justify-between w-full h-16">
-                        <div class="flex items-center">
-                            <a class="flex" href="{{ route('home') }}">
-                                <div class="flex items-center font-semibold tracking-wider text-gray-900 uppercase transition-colors dark:text-white hover:text-orange-400 focus:text-orange-400 focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-orange-500 focus-visible:ring-opacity-60 focus-visible:outline-none focus:outline-none">{{ config('app.name') }}</div>
-                            </a>
-                        </div>
-                        <div class="hidden sm:block">
-                            <div class="flex items-baseline ml-auto space-x-3">
-                                @if(config('blog.uses'))<x-nav-link-front href="{{ route('uses') }}">My Setup</x-nav-link-front>@endif
-                                @if(\App\Models\Post::isPublished()->count() > 0)<x-nav-link-front href="{{ route('blog.index') }}">Blog</x-nav-link-front>@endif
+    <body class="flex h-full bg-slate-50 dark:bg-slate-900 {{ config('app.debug') ? 'debug-screens' : '' }}">
+        <div class="flex w-full my-12">
+{{--            <div class="fixed inset-0 flex justify-center sm:px-8">
+                <div class="flex w-full max-w-7xl lg:px-8">
+                    <div class="w-full bg-white ring-1 ring-slate-100 dark:bg-slate-900 dark:ring-slate-300/20"></div>
+                </div>
+            </div>--}}{{--            <div class="fixed inset-0 flex justify-center sm:px-8">
+                <div class="flex w-full max-w-7xl lg:px-8">
+                    <div class="w-full bg-white ring-1 ring-slate-100 dark:bg-slate-900 dark:ring-slate-300/20"></div>
+                </div>
+            </div>--}}
+            <div class="relative flex w-full flex-col">
+                {{--<nav x-data="{ open: false }" class="px-4 bg-white md:px-8 dark:bg-gray-900">
+                    <div class="max-w-4xl mx-auto">
+                        <div class="flex items-center justify-between w-full h-16">
+                            <div class="flex items-center">
+                                <a class="flex" href="{{ route('home') }}">
+                                    <div class="flex items-center font-semibold tracking-wider text-gray-900 uppercase transition-colors dark:text-white hover:text-orange-400 focus:text-orange-400 focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-orange-500 focus-visible:ring-opacity-60 focus-visible:outline-none focus:outline-none">{{ config('app.name') }}</div>
+                                </a>
+                            </div>
+                            <div class="hidden sm:block">
+                                <div class="flex items-baseline ml-auto space-x-3">
+                                    @if(config('blog.uses'))<x-nav-link-front href="{{ route('uses') }}">My Setup</x-nav-link-front>@endif
+                                    @if(\App\Models\Post::isPublished()->count() > 0)<x-nav-link-front href="{{ route('blog.index') }}">Blog</x-nav-link-front>@endif
+                                </div>
+                            </div>
+                            <div class="flex -mr-2 sm:hidden">
+                                <button @click="open = !open"
+                                    class="inline-flex items-center justify-center p-2 text-gray-800 rounded-md dark:text-white hover:text-orange-400 focus:outline-none">
+                                    <svg xmlns="http://www.w3.org/2000/svg" class="w-8 h-8" fill="none" viewBox="0 0 24 24"
+                                        stroke="currentColor">
+                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                            d="M4 6h16M4 12h16M4 18h16" />
+                                    </svg>
+                                </button>
                             </div>
                         </div>
-                        <div class="flex -mr-2 sm:hidden">
-                            <button @click="open = !open"
-                                class="inline-flex items-center justify-center p-2 text-gray-800 rounded-md dark:text-white hover:text-orange-400 focus:outline-none">
-                                <svg xmlns="http://www.w3.org/2000/svg" class="w-8 h-8" fill="none" viewBox="0 0 24 24"
-                                    stroke="currentColor">
-                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                        d="M4 6h16M4 12h16M4 18h16" />
-                                </svg>
-                            </button>
+                    </div>
+                    <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
+                        <div class="px-2 pt-2 pb-3 space-y-1 sm:px-3">
+                            @if(config('blog.uses'))<x-nav-link-responsive-front href="{{ route('uses') }}">My Setup</x-nav-link-responsive-front>@endif
+                            @if(\App\Models\Post::isPublished()->count() > 0)<x-nav-link-responsive-front href="{{ route('blog.index') }}">Blog</x-nav-link-responsive-front>@endif
                         </div>
                     </div>
-                </div>
-                <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
-                    <div class="px-2 pt-2 pb-3 space-y-1 sm:px-3">
-                        @if(config('blog.uses'))<x-nav-link-responsive-front href="{{ route('uses') }}">My Setup</x-nav-link-responsive-front>@endif
-                        @if(\App\Models\Post::isPublished()->count() > 0)<x-nav-link-responsive-front href="{{ route('blog.index') }}">Blog</x-nav-link-responsive-front>@endif
+                </nav>--}}
+                <main class="flex-auto mx-auto w-full max-w-7xl px-4 sm:px-8 lg:px-12">
+                    {{ $slot }}
+                </main>
+                {{--<div class="pb-6 mt-12 sm:mt-24">
+                    <div class="max-w-4xl mx-auto text-xs text-gray-400 sm:px-4 lg:px-0">
+                        <div class="pb-4 mb-2 border-t border-gray-100 dark:border-gray-800"></div>
+                        <div class="grid grid-flow-row grid-cols-3 gap-y-2 gap-x-1 lg:gap-x-0.5 font-medium sm:grid-flow-col sm:grid-rows-auto sm:grid-cols-6 md:grid-rows-1 lg:grid-cols-8">
+                            @foreach (\App\Models\Social::all() as $social)
+                                <div class="text-center sm:text-left">
+                                    <a href="{{$social->link}}" class="transition-colors hover:text-orange-500 focus:text-orange-500 focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-orange-500 focus-visible:ring-opacity-60 focus-visible:outline-none focus:outline-none" target="_blank"><i class="{{$social->icon}} text-sm mr-1"></i>{{$social->name}}</a>
+                                </div>
+                            @endforeach
+                        </div>
+                        <div class="justify-center mx-auto my-6 text-center sm:text-left">
+                            <a href="https://github.com/dsbilling/dsbilling.no" class="text-gray-400 hover:text-gray-500 dark:text-gray-500 dark:hover:text-gray-400">Built with Laravel and Tailwind, and it's open-source on <i class="fab fa-github"></i>Github</a>
+                        </div>
                     </div>
-                </div>
-            </nav>
-            <main class="max-w-4xl mx-auto mt-8 antialiased sm:px-4 sm:mt-16 lg:px-0">
-                {{ $slot }}
-            </main>
-            <div class="pb-6 mt-12 sm:mt-24">
-                <div class="max-w-4xl mx-auto text-xs text-gray-400 sm:px-4 lg:px-0">
-                    <div class="pb-4 mb-2 border-t border-gray-100 dark:border-gray-800"></div>
-                    <div class="grid grid-flow-row grid-cols-3 gap-y-2 gap-x-1 lg:gap-x-0.5 font-medium sm:grid-flow-col sm:grid-rows-auto sm:grid-cols-6 md:grid-rows-1 lg:grid-cols-8">
-                        @foreach (\App\Models\Social::all() as $social)
-                            <div class="text-center sm:text-left">
-                                <a href="{{$social->link}}" class="transition-colors hover:text-orange-500 focus:text-orange-500 focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-orange-500 focus-visible:ring-opacity-60 focus-visible:outline-none focus:outline-none" target="_blank"><i class="{{$social->icon}} text-sm mr-1"></i>{{$social->name}}</a>
-                            </div>
-                        @endforeach
-                    </div>
-                    <div class="justify-center mx-auto my-6 text-center sm:text-left">
-                        <a href="https://github.com/dsbilling/dsbilling.no" class="text-gray-400 hover:text-gray-500 dark:text-gray-500 dark:hover:text-gray-400">Built with Laravel and Tailwind, and it's open-source on <i class="fab fa-github"></i>Github</a>
-                    </div>
-                </div>
+                </div>--}}
             </div>
         </div>
 
