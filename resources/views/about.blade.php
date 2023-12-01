0ea5713280312c068470bfda50af89ee945b986e
Daniel S. Billing <daniel@izy.no>
2023-12-01 10:57:44 +0100

    Keep this for later

---
+++ resources/views/about.blade.php
@@ -0,0 +1,57 @@
+
+        {{--<section id="about">
+            <div class="space-x-0 sm:space-x-5 lg:flex item-center lg:-mx-4">
+                <div>
+                    <h1 class="text-3xl font-bold text-gray-900 dark:text-gray-50 lg:text-4xl">Hi there, I'm Daniel
+                        &#x1F44B;</h1>
+                    <div class="mt-4 text-gray-800 dark:text-gray-100">
+                        <p>I currently work as a
+                            <x-gradient-text>Backend Lead</x-gradient-text>
+                            for
+                            <x-link-text link="https://izy.no" noreferrer="true">Izy AS</x-link-text>
+                            , where we develop platform for landlords to take care of tenants' needs in a digital
+                            everyday life, with an associated mobile application for communication, trade, information
+                            and booking.
+                        </p>
+                        <p class="mt-2">In January 2022, I started my own company called
+                            <x-link-text link="https://kilobyte.no">
+                                <x-gradient-text>Kilobyte AS</x-gradient-text>
+                            </x-link-text>
+                            , a technology and development company. Before this I had a sole proprietorship for 10
+                            years.
+                        </p>
+                        <p class="mt-2">I previously worked 10 years at
+                            <x-link-text link="https://intility.no" noreferrer="true">Intility AS</x-link-text>
+                            as a
+                            <x-gradient-text>Systems Developer</x-gradient-text>
+                            in the
+                            <x-gradient-text>AV</x-gradient-text>
+                            department &mdash; focusing on developing an internal portal, but I also do develop
+                            solutions for digital signage as well as programming meeting rooms, auditoriums, and
+                            operation centers with
+                            <x-link-text link="https://crestron.com" noreferrer="true">Crestron</x-link-text>
+                            .
+                        </p>
+                        <p class="mt-2">Other than that I do
+                            <x-gradient-text>freelance work</x-gradient-text>
+                            for
+                            <x-gradient-text>e-sport</x-gradient-text>
+                            teams,
+                            <x-gradient-text>LAN-parties</x-gradient-text>
+                            ,
+                            <x-gradient-text>games</x-gradient-text>
+                            and
+                            <x-gradient-text>digital communities</x-gradient-text>
+                            . <span class="pb-0 border-b border-gray-400 border-dashed dark:border-gray-500">I love making websites that are usable, simple, and user-friendly.</span>
+                        </p>
+                        <p class="mt-2">If you want to get in touch with me use one of my socials.
+                            <x-gradient-text>Say hi!</x-gradient-text>
+                        </p>
+                    </div>
+                </div>
+                <div class="items-center flex-shrink-0 mx-auto mt-12 lg:px-4 lg:mt-0 sm:mx-0">
+                    <img alt="Profile" src="{{ asset('img/daniel.jpg') }}"
+                         class="flex w-56 h-56 mx-auto rounded-full sm:mx-0">
+                </div>
+            </div>
+        </section>--}}
