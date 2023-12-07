806360c6f823a49aa73fb23e693041945b88ccd8
Daniel S. Billing <daniel@izy.no>
2023-12-07 19:52:01 +0100

    Update nav-link-front.blade.php

--- resources/views/components/nav-link-front.blade.php
+++ resources/views/components/nav-link-front.blade.php
@@ -2,10 +2,13 @@
 
 @php
 $classes = ($active ?? false)
-            ? 'px-3 py-2 transition-colors text-red-200 dark:text-red-100 hover:text-red-400 focus:text-red-400 focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-blue-500 focus-visible:ring-opacity-60 focus-visible:outline-none focus:outline-none'
-            : 'px-3 py-2 transition-colors text-gray-800 dark:text-gray-100 hover:text-red-400 focus:text-red-400 focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-blue-500 focus-visible:ring-opacity-60 focus-visible:outline-none focus:outline-none';
+            ? 'relative block px-3 py-2 transition text-orange-500 dark:text-orange-400 font-bold'
+            : 'relative block px-3 py-2 transition hover:text-orange-500 dark:hover:text-orange-400';
 @endphp
-    
+
 <a {{ $attributes->merge(['class' => $classes]) }}>
     {{ $slot }}
+    @if($active ?? false)
+        <span class="absolute inset-x-1 -bottom-px h-px bg-orange-500"></span>
+    @endif
 </a>
