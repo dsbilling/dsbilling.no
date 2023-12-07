ae35fc8bb9c4f36bdb347b8d9239d14516c67432
Daniel S. Billing <daniel@izy.no>
2023-12-07 21:08:44 +0100

    Reworked the blog index page

--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -1,33 +1,46 @@
+@php use Illuminate\Foundation\Inspiring; @endphp
 <x-guest-layout>
     <h1 class="text-3xl font-bold text-gray-900 dark:text-gray-50 lg:text-4xl">
         <x-gradient-text>Blog</x-gradient-text>
+        <p class="mt-2 text-sm text-gray-500 dark:text-gray-400">A collection of my thoughts and writings.</p>
     </h1>
-	<div class="flex flex-col items-center">
-		<div class="grid gap-4 pt-8 sm:gap-6 md:grid-cols-2 w-full">
-			@forelse($posts as $post)
-				<div class="w-full h-full m-auto overflow-hidden border border-gray-200 rounded-lg shadow-lg cursor-pointer bg-gray-50/50 hover:bg-gray-50 dark:border-gray-700 group dark:bg-gray-800/50 hover:dark:bg-gray-800 dark:shadow-gray-100/10">
-					<a href="{{ route('blog.show', $post->slug) }}" class="block w-full h-full">
-						{{--<img alt="blog photo" src="{{ asset('img/daniel.jpg') }}" class="object-cover w-full max-h-28"/>--}}
-						<div class="w-full p-4 ">
-							<p class="text-sm font-medium text-orange-500 dark:text-orange-400">
-								{{ now()->subMonth(1) > $post->published_at ? $post->published_at->isoFormat('D. MMMM YYYY') : $post->published_at->diffForHumans() }} &middot; {{ read_time($post->body)}} &middot;  {{ App\Helpers\NumberHelper::nearestK(views($post)->count()) }} {{ Str::plural('view', views($post)->count()) }} &middot; {{ $post->likes_count }} {{ Str::plural('like', $post->likes_count) }}
-							</p>
-							<p class="my-2 text-2xl font-semibold text-gray-800 sm:text-3xl dark:text-white">
-								{{ $post->title }}
-							</p>
-							<p class="text-sm font-light text-gray-500 dark:text-gray-300">
-								{!! strip_tags(Str::of(Str::limit($post->body, 500))->markdown()) !!}
-							</p>
-						</div>
-					</a>
-				</div>
-			@empty
-				<div class="w-full m-auto text-center">
-					<x-gradient-text>{{ \Illuminate\Foundation\Inspiring::quote() }}</x-gradient-text>
-				</div>
-			@endforelse
-		</div>
-	</div>
+    <div class="mt-16 sm:mt-20">
+        <div class="md:border-l md:border-orange-200 md:pl-6 md:dark:border-orange-700/40">
+            <div class="flex max-w-3xl flex-col space-y-16">
+                @forelse($posts as $post)
+                    <article class="md:grid md:grid-cols-4 md:items-baseline">
+                        <div class="md:col-span-3 group relative flex flex-col items-start">
+                            <time
+                                class="md:hidden relative z-10 order-first mb-3 flex items-center text-sm text-orange-500 dark:text-orange-500 pl-3.5"
+                                datetime="2022-09-05"><span class="absolute inset-y-0 left-0 flex items-center"
+                                                            aria-hidden="true"><span
+                                        class="h-4 w-0.5 rounded-full bg-orange-200 dark:bg-orange-500"></span></span>{{ now()->subMonth(1) > $post->published_at ? $post->published_at->isoFormat('D. MMMM YYYY') : $post->published_at->diffForHumans() }}
+                            </time>
+                            <h2 class="text-2xl font-semibold tracking-tight text-slate-800 dark:text-slate-100">
+                                <div
+                                    class="absolute -inset-x-4 -inset-y-6 z-0 scale-95 bg-slate-50 opacity-0 transition group-hover:scale-100 group-hover:opacity-100 dark:bg-slate-800/50 sm:-inset-x-6 sm:rounded-2xl"></div>
+                                <a href="{{ route('blog.show', $post->slug) }}"><span
+                                        class="absolute -inset-x-4 -inset-y-6 z-20 sm:-inset-x-6 sm:rounded-2xl"></span><span
+                                        class="relative z-10">{{ $post->title }}</span></a>
+                            </h2>
+                            <p class="relative z-10 mt-2 text-sm text-slate-600 dark:text-slate-300">{!! strip_tags(Str::of(Str::limit($post->body, 500))->markdown()) !!}</p>
+                            <p class="relative z-10 mt-4 text-slate-600 dark:text-slate-400 text-xs">
+                                {{ read_time($post->body)}} &middot;  {{ App\Helpers\NumberHelper::nearestK(views($post)->count()) }} {{ Str::plural('view', views($post)->count()) }} &middot; {{ $post->likes_count }} {{ Str::plural('like', $post->likes_count) }}
+                            </p>
+                        </div>
+                        <time
+                            class="mt-1 hidden md:block relative z-10 order-first mb-3 items-center text-sm text-orange-500 dark:text-orange-500"
+                            datetime="2022-09-05">{{ now()->subMonth(1) > $post->published_at ? $post->published_at->isoFormat('D. MMMM YYYY') : $post->published_at->diffForHumans() }}
+                        </time>
+                    </article>
+                @empty
+                    <div class="w-full m-auto text-center">
+                        <x-gradient-text>{{ Inspiring::quote() }}</x-gradient-text>
+                    </div>
+                @endforelse
+            </div>
+        </div>
+    </div>
     @if(!$posts->isEmpty())
         <div class="mt-8 w-full m-auto">
             {!! $posts->links() !!}
