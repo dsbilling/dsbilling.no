f542537e1724b79ffca0e05c610ccc75f014fd2b
Daniel S. Billing <daniel@izy.no>
2023-12-07 21:08:25 +0100

    Fixed cv button for smaller devices

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -49,7 +49,7 @@
                         <x-gradient-text>Say hi!</x-gradient-text>
                     </p>
                     <p class="mt-10">If you are interested in me and my experience, you can check out my <x-link-text link="https://linkedin.com/in/dsbilling" noreferrer="true">LinkedIn</x-link-text> or download my CV below.</p>
-                    <a class="inline-flex items-center gap-2 justify-center rounded-md py-2 px-3 text-sm outline-offset-2 transition active:transition-none bg-gradient-to-r from-orange-400 to-orange-600 font-bold text-white group mt-6 hover:bg-gradient-to-br hover:shadow-md w-1/4"
+                    <a class="inline-flex items-center gap-2 justify-center rounded-md py-2 px-3 text-sm outline-offset-2 transition active:transition-none bg-gradient-to-r from-orange-400 to-orange-600 font-bold text-white group mt-6 hover:bg-gradient-to-br hover:shadow-md w-1/2 sm:w-1/3 md:w-1/4"
                        href="{{ route('cv') }}">Download my CV
                         <svg aria-hidden="true"  xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="h-4 w-4">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5M16.5 12L12 16.5m0 0L7.5 12m4.5 4.5V3" />
