28480b4f01bd716844b34e4199885d8de1b8b821
Daniel S. Billing <daniel@izy.no>
2023-12-09 21:22:26 +0100

    Added socials to filament

---
+++ app/Filament/Resources/SocialResource.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\SocialResource\Pages;
+use App\Models\Social;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+
+class SocialResource extends Resource
+{
+    protected static ?string $model = Social::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-link';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('name')
+                    ->autofocus()
+                    ->required()
+                    ->unique(Social::class, 'name')
+                    ->label(__('Name')),
+                Forms\Components\TextInput::make('icon')
+                    ->required()
+                    ->label(__('Icon')),
+                Forms\Components\TextInput::make('link')
+                    ->required()
+                    ->label(__('Link')),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('name')
+                    ->sortable()
+                    ->searchable()
+                    ->label(__('Name')),
+                Tables\Columns\TextColumn::make('icon')
+                    ->sortable()
+                    ->searchable()
+                    ->label(__('Icon')),
+                Tables\Columns\TextColumn::make('link')
+                    ->sortable()
+                    ->searchable()
+                    ->label(__('Link')),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListSocials::route('/'),
+            'create' => Pages\CreateSocial::route('/create'),
+            'edit' => Pages\EditSocial::route('/{record}/edit'),
+        ];
+    }
+}
