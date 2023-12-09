c74c344cb4ce3a2d2617c7acaf6ae9ccb4360305
Daniel S. Billing <daniel@izy.no>
2023-12-09 21:14:08 +0100

    Code styling

--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -3,17 +3,13 @@
 namespace App\Filament\Resources;
 
 use App\Filament\Resources\UserResource\Pages;
-use App\Filament\Resources\UserResource\RelationManagers;
 use App\Models\User;
-use Filament\Forms;
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
 use Filament\Tables;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
-use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Database\Eloquent\SoftDeletingScope;
 
 class UserResource extends Resource
 {
@@ -80,7 +76,6 @@ class UserResource extends Resource
                     ->default('')
                     ->searchable()
                     ->sortable(),
-
 
             ])
             ->filters([
