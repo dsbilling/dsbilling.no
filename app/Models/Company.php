50a26e7cc791a73c9c148dda40f33ba5ed6a5f93
Daniel S. Billing <daniel@izy.no>
2023-12-09 21:14:21 +0100

    Added missing relation to company model

--- app/Models/Company.php
+++ app/Models/Company.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Company extends Model
@@ -16,8 +17,13 @@ class Company extends Model
     /**
      * Get the certifications for the company.
      */
-    public function certifications()
+    public function certifications(): HasMany
     {
         return $this->hasMany(Certification::class);
+    }
+
+    public function courses(): HasMany
+    {
+        return $this->hasMany(Course::class);
     }
 }
