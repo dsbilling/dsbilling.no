92dd77cb57169cc3202749177a92f4221ac23051
Daniel S. Billing <daniel@kilobyte.no>
2024-09-18 16:17:28 +0200

    Removed unused packages

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,17 +6,3 @@ Alpine.start();
 
 import '@fortawesome/fontawesome-free/js/all.js';
 
-import flatpickr from "flatpickr";
-import "flatpickr/dist/themes/dark.css";
-
-flatpickr(".flatpickrSelector", {
-    enableTime: true,
-    dateFormat: "Y-m-d H:i:S",
-    time_24hr: true,
-});
-
-import EasyMDE from 'easyMDE';
-const easyMDE = new EasyMDE({ 
-    element: document.getElementById("body"),
-    maxHeight: "1000px",
-});
