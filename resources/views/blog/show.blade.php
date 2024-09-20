d584261a5e8b00ca0399adf811327a13119d2053
Daniel S. Billing <daniel@kilobyte.no>
2024-09-20 23:57:23 +0200

    Updated styling of "old post"-alert

--- resources/views/blog/show.blade.php
+++ resources/views/blog/show.blade.php
@@ -20,17 +20,17 @@
         </p>
 
         @if (now()->subYears(1) > $post->published_at)
-            <div class="rounded-md bg-blue-50 p-4 mt-8">
+            <div class="rounded-md p-4 mt-8 font-semibold bg-blue-50 dark:bg-blue-900 text-blue-800 dark:text-blue-100 border border-blue-200 dark:border-blue-700">
                 <div class="flex">
                     <div class="flex-shrink-0">
-                        <svg class="h-5 w-5 text-blue-400" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                        <svg class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
                             <path fill-rule="evenodd"
                                   d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a.75.75 0 000 1.5h.253a.25.25 0 01.244.304l-.459 2.066A1.75 1.75 0 0010.747 15H11a.75.75 0 000-1.5h-.253a.25.25 0 01-.244-.304l.459-2.066A1.75 1.75 0 009.253 9H9z"
                                   clip-rule="evenodd"/>
                         </svg>
                     </div>
                     <div class="ml-3 flex-1 md:flex md:justify-between">
-                        <p class="text-sm text-blue-700">{{ __('Note: This article was published over a year ago. Information within may have changed since then. While efforts are made to keep content current, please verify critical details before making decisions based on this information.') }}</p>
+                        <p class="text-sm">{{ __('Note: This article was published over a year ago. Information within may have changed since then. While efforts are made to keep content current, please verify critical details before making decisions based on this information.') }}</p>
                     </div>
                 </div>
             </div>
